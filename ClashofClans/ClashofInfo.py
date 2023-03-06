239c1cb20c8e6d3ee7dd411f8512125c95684348
new92 <94779840+new92@users.noreply.github.com>
2023-03-06 12:09:42 +0200

    Update ClashofInfo.py

--- ClashofClans/ClashofInfo.py
+++ ClashofClans/ClashofInfo.py
@@ -64,7 +64,7 @@ def ProgInfo():
     forks = 4
     print("[+] Author: "+author)
     print("[+] Github: @"+author)
-    print("[+] License: "+stlicense)
+    print("[+] License: "+slicense)
     print("[+] Programming language(s) used: "+str(lang))
     print("[+] Language(s): "+str(language))
     print("[+] Program's name: "+str(name))
