e5f30c1be9e1a868810a59b22c7f82bcf95fdaee
new92 <94779840+new92@users.noreply.github.com>
2023-03-14 12:46:29 +0200

    Update RoyaleInfo.py

--- ClashRoyale/RoyaleInfo.py
+++ ClashRoyale/RoyaleInfo.py
@@ -72,7 +72,7 @@ def ProgInfo():
     print(f"[+] Natural Language: {lang}")
     print(f"[+] Program's name: {name}")
     print(f"[+] Programming language(s) used: "+)
-    print("[+] License: "+license1)
+    print("[+] License: "+lice)
     print("[+] API used: "+str(api))
     print("[+] URL: "+api_url)
     print("[+] Number of lines: "+str(lines))
