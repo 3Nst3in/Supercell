455a4129043cda394098f447efe76710d45ea239
new92 <94779840+new92@users.noreply.github.com>
2023-03-16 22:14:31 +0200

    Update RoyaleInfo.py

--- ClashRoyale/RoyaleInfo.py
+++ ClashRoyale/RoyaleInfo.py
@@ -77,7 +77,7 @@ def ProgInfo():
     print(f"[+] URL: {api_url}")
     print(f"[+] Number of lines: {lines}")
     print(f"[+] File size: {fsize} bytes")
-    print(f"[+] Github repository stars: {stars}"+)
+    print(f"[+] Github repository stars: {stars}")
     print("[+] Github repository forks: "+str(forks))
 
 def banner():
