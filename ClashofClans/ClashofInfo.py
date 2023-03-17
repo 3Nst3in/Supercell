e8473b95589e7d7db06e73faaa3258e8ce2aa8f1
new92 <94779840+new92@users.noreply.github.com>
2023-03-17 15:28:00 +0200

    Update ClashofInfo.py

--- ClashofClans/ClashofInfo.py
+++ ClashofClans/ClashofInfo.py
@@ -195,7 +195,7 @@ def Player(tag: str):
             maxedh = 0
             for i in range(len(js['heroes'])):
                 print(f"[+] Name: {js['heroes'][i]['name']}")
-                print(f"[+] Level: {js['heroes'][i]['level']}"+str(js['heroes'][i]['level']))
+                print(f"[+] Level: {js['heroes'][i]['level']}")
                 print("[+] Max level: "+str(js['heroes'][i]['maxLevel']))
                 if js['heroes'][i]['maxLevel'] == js['heroes'][i]['level']:
                     maxedh += 1
